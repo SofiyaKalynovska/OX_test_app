baf7d3bff6f40f390b201958dba8ceb5d63f8410
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-14 00:28:37 +0200

    Created filter slice for tabs and pagination

---
+++ src/redux/filterSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface FilterState {
+  activeTab: "api" | "my"; 
+  filters: {
+    [key: string]: boolean; 
+  };
+  limit: number;
+}
+
+const initialState: FilterState = {
+  activeTab: "api",
+  filters: {
+    isPublished: true, 
+  },
+  limit: 8, 
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setActiveTab(state, action: PayloadAction<"api" | "my">) {
+      state.activeTab = action.payload;
+    },
+
+    setFilter(state, action: PayloadAction<{ key: string; value: boolean }>) {
+      const { key, value } = action.payload;
+      state.filters[key] = value;
+    },
+
+    setLimit(state, action: PayloadAction<number>) {
+      state.limit = action.payload;
+    },
+
+    resetFilters(state) {
+      state.filters = initialState.filters;
+      state.limit = initialState.limit;
+    },
+  },
+});
+
+export const { setActiveTab, setFilter, setLimit, resetFilters } =
+  filterSlice.actions;
+
+export default filterSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./productSlice";
+import filterReducer from "./filterSlice";
 
 const store = configureStore({
   reducer: {
-    products: productsReducer, 
+    products: productsReducer,
+    filter: filterReducer,
   },
 });
 
