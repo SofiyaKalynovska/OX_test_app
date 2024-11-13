69a8a8a6ef36ab25741523ed2e33f77e65a03c93
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-13 15:14:07 +0200

    Added routes

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+const BASE_URL = "https://fakestoreapi.com/products";
+
+export const routes = {
+  BASE_URL, 
+
+  //Pages
+  products: "/products",
+  productDetails: "/products/:id",
+  createProduct: "/create-product",
+};
