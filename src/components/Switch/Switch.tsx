a7bbf599e346925e7f996d140224bf31eaa98288
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-14 00:10:28 +0200

    Created reusable switch

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface SwitchProps {
+  label: string;
+  checked: boolean | undefined;
+  onChange: (checked: boolean) => void;
+}
+
+ const Switch: React.FC<SwitchProps> = ({ label, checked, onChange }) => {
+  return (
+    <div className="flex items-center">
+      <span className="mr-2">{label}</span>
+      <label className="relative inline-block w-12 h-6">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => onChange(e.target.checked)} 
+          className="opacity-0 w-0 h-0"
+        />
+        <span
+          className={`absolute inset-0 flex items-center justify-between cursor-pointer rounded-full bg-gray-300 transition duration-300 ease-in-out ${
+            checked ? "bg-green-500" : "bg-gray-300"
+          }`}
+        >
+          <span
+            className={`block w-6 h-6 bg-white rounded-full transition duration-300 ease-in-out transform ${
+              checked ? "translate-x-6" : ""
+            }`}
+          ></span>
+        </span>
+      </label>
+    </div>
+  );
+};
+
+export default Switch;
