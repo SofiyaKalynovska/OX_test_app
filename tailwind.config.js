c69afe5f153da9bf1e0bbb2356829656a0729b0c
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-13 13:20:56 +0200

    Created base structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#1e3a8a',
+        'light-blue': '#3b82f6',
+      },
+    },
+  },
+  plugins: [],
+}
+
