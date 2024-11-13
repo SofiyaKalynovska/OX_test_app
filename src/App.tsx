bbae9966dfbdae10bb7cfbdd5fa00b3c9a8782f9
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-14 00:24:04 +0200

    Added create product page to router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import { Layout } from "./components/Layout";
-import ProductsListPage from "./pages/ProductsListPage";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import ProductDetailPage from "./pages/ProductDetailsPage";
 import ProductAddPage from "./pages/ProductAddPage";
+import ProductsListPage from "./pages/ProductsListPage";
  
 
 const App: React.FC = () => {
