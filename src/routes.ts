fcbc36e1553765fbb0ce00b931171464575b5185
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-13 18:03:29 +0200

    Added fetch logic to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 const BASE_URL = "https://fakestoreapi.com/products";
 
 export const routes = {
-  BASE_URL, 
+  // API Routes
+  fetchAllProducts: `${BASE_URL}`, 
+  fetchProductDetails: (id: number) => `${BASE_URL}/${id}`,
 
-  //Pages
-  products: "/products",
-  productDetails: "/products/:id",
-  createProduct: "/create-product",
+  // Pages
+  products: "/products", 
+  productDetails: "/products/:id", 
+  createProduct: "/create-product", 
 };
