6369dfd0233e7311195d5279e04561641224b6a1
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-13 18:59:41 +0200

    Created store

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productsReducer from "./productSlice";
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer, 
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
