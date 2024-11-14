c746a3da0d2a833717cbe24cb8fe84846162e47f
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-14 18:31:50 +0200

    Added types to validation schema

--- src/validation/createProductValidationSchema.ts
+++ src/validation/createProductValidationSchema.ts
@@ -1,16 +1,24 @@
 import * as Yup from "yup";
 
-const createProductValidationSchema = Yup.object().shape({
-  title: Yup.string()
-    .required("Title is required")
-    .min(3, "Title must be at least 3 characters"),
-  price: Yup.number()
-    .required("Price is required")
-    .positive("Price must be a positive number"),
-  description: Yup.string()
-    .required("Description is required")
-    .min(10, "Description must be at least 10 characters"),
-  category: Yup.string().required("Category is required"),
-});
+export interface ProductFormValues {
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+}
+
+const createProductValidationSchema: Yup.ObjectSchema<ProductFormValues> =
+  Yup.object({
+    title: Yup.string()
+      .required("Title is required")
+      .min(3, "Title must be at least 3 characters"),
+    price: Yup.number()
+      .required("Price is required")
+      .positive("Price must be a positive number"),
+    description: Yup.string()
+      .required("Description is required")
+      .min(10, "Description must be at least 10 characters"),
+    category: Yup.string().required("Category is required"),
+  });
 
 export default createProductValidationSchema;
