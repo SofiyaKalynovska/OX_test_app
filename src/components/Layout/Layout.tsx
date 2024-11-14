dfe0662642c2a098218e1e045be2a6213dee022b
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-14 21:55:21 +0200

    Changed styles for layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,13 +4,13 @@ import { Target } from "lucide-react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col min-w-screen bg-white">
+    <div className="flex flex-col bg-white min-w-screen min-h-screen">
       <Navbar />
-      <main className="flex-1 p-6">{children}</main>
-      <footer className="bg-dark-blue text-white p-4 text-center  w-full">
-        <div className="flex gap-1 items-center justify-center">
+      <main className="flex-1">{children}</main>
+      <footer className="bg-dark-blue p-4 w-full text-center text-white">
+        <div className="flex justify-center items-center gap-1">
           <Target />
-          <h1 className="text-xl font-semibold">PMA</h1>
+          <h1 className="font-semibold text-xl">PMA</h1>
         </div>
         <p>Thanks to this platform, you can easily manage products</p>
         <p>&copy; 2024 | All rights reserved</p>
