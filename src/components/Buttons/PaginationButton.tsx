60d02ec477b58d81efdc8f876609457c3102b76f
Sofiya Kalynovska <sofiyakalynovska@gmail.com>
2024-11-15 00:43:45 +0200

    Changed styles to pagination buttons

--- src/components/Buttons/PaginationButton.tsx
+++ src/components/Buttons/PaginationButton.tsx
@@ -17,8 +17,8 @@ const ProductListPaginationButton: React.FC<PaginationButtonProps> = ({
     <button
       onClick={onClick}
       className={`px-4 py-2 rounded-md ${
-        active ? "bg-main-orange" : "bg-gray-200"
-      } transition-colors duration-300`}
+        active ? "bg-main-orange text-white" : "bg-gray-200"
+      } transition-colors duration-300 hover:bg-orange-hover`}
     >
       {label}
     </button>
